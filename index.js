5fe8857f7168c414444d0a72dbab6dcff31bd665
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2018-07-19 13:56:01 -0300

    Use reject instead of throw.

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const fs = require("fs");
 const Inliner = require("inliner");
 
 module.exports = bundler => {
-  bundler.on("bundled", async (bundle) => {
+  bundler.on("bundled", (bundle) => {
     if (process.env.NODE_ENV !== "production")
       return;
 
-    await new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       const inliner = new Inliner(bundle.name, function(error, html) {
         if (error) {
-          throw error;
+          reject(error);
         }
         fs.writeFile(bundle.name, html, function(err) {
           if (error) {
-            throw error;
+            reject(error);
           }
           resolve();
         });
