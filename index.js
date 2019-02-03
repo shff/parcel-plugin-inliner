c6a1960703401b01705e8405ee1c00a738681116
Silvio Henrique Ferreira <shferreira@me.com>
2019-02-02 23:11:45 -0200

    Move bundling code to a different function

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const fs = require("fs");
 const Inliner = require("inliner");
 
-module.exports = bundler => {
-  bundler.on("bundled", (bundle) => {
-    if ((process.env.NODE_ENV !== "production" || bundle.entryAsset.type !== "html")
-      && !process.env.INLINE_ASSETS)
-      return;
+const run = bundle => {
+  if (bundle.entryAsset.type !== "html")
+    return;
 
-    return new Promise((resolve, reject) => {
-      const inliner = new Inliner(bundle.name, function(error, html) {
+  return new Promise((resolve, reject) => {
+    const inliner = new Inliner(bundle.name, function(error, html) {
+      if (error) {
+        reject(error);
+      }
+      fs.writeFile(bundle.name, html, function(err) {
         if (error) {
           reject(error);
         }
-        fs.writeFile(bundle.name, html, function(err) {
-          if (error) {
-            reject(error);
-          }
-          resolve();
-        });
+        resolve();
       });
     });
+  });
+};
+
+module.exports = bundler => {
+  bundler.on("bundled", (bundle) => {
+    if (process.env.NODE_ENV !== "production" && !process.env.INLINE_ASSETS)
+      return;
+
+    return run(bundle);
   });
 };
