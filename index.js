7b4699309abad9c31479ba0c6cd68390dbd7569b
Silvio Henrique Ferreira <shferreira@me.com>
2019-03-01 01:35:32 -0300

    Simplify code

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const fs = require("fs");
 const postHTML = require("posthtml");
 const posthtmlInlineAssets = require("posthtml-inline-assets");
 
-const run = async bundle => {
-  if (bundle.entryAsset.type !== "html") return;
-
-  const cwd = bundle.entryAsset.options.outDir;
-  const data = fs.readFileSync(bundle.name);
-  const result = await postHTML([posthtmlInlineAssets({ cwd })]).process(data);
-  fs.writeFileSync(bundle.name, result.html);
-};
-
 module.exports = bundler => {
   bundler.on("bundled", (bundle) => {
-    if (process.env.NODE_ENV !== "production" && !process.env.INLINE_ASSETS)
-      return;
+    if (process.env.NODE_ENV !== "production" && !process.env.INLINE_ASSETS) return;
 
-    if (!bundle.entryAsset)
-      return Promise.all(Array.from(bundle.childBundles).map((child) => run(child)));
-
-    return run(bundle);
+    const bundles = Array.from(bundle.childBundles).concat([bundle]);
+    return Promise.all(bundles.map(async bundle => {
+      if (!bundle.entryAsset || bundle.entryAsset.type !== "html") return;
+    
+      const cwd = bundle.entryAsset.options.outDir;
+      const data = fs.readFileSync(bundle.name);
+      const result = await postHTML([posthtmlInlineAssets({ cwd })]).process(data);
+      fs.writeFileSync(bundle.name, result.html);
+    }));
   });
 };
