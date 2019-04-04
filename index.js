fd7378dc5a1fb4003bdf78dfc74de1744575b892
Romain Racamier <romain.racamier@gmail.com>
2019-04-04 16:02:55 +0200

    fix for img with src that link to a svg file

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const fs = require("fs");
 const postHTML = require("posthtml");
 const posthtmlInlineAssets = require("posthtml-inline-assets");
 
+const transforms = {
+  imgSvg: {
+    resolve (node) {
+      const isImg = node.tag === 'img'
+      const path = node.attrs && node.attrs.src
+      if (!isImg || !path) {
+        return
+      }
+      return (path.indexOf('.svg') > -1) ? path : null;
+    },
+    transform (node, data) {
+      const mime = 'image/svg+xml';
+      node.attrs.src = 'data:' + mime + ';base64,' + data.buffer.toString('base64');
+    }
+  }
+}
+
 module.exports = bundler => {
   bundler.on("bundled", (bundle) => {
     const bundles = Array.from(bundle.childBundles).concat([bundle]);
@@ -10,7 +27,7 @@ module.exports = bundler => {
     
       const cwd = bundle.entryAsset.options.outDir;
       const data = fs.readFileSync(bundle.name);
-      const result = await postHTML([posthtmlInlineAssets({ cwd })]).process(data);
+      const result = await postHTML([posthtmlInlineAssets({ cwd, transforms })]).process(data);
       fs.writeFileSync(bundle.name, result.html);
     }));
   });
