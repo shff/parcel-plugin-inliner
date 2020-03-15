cf3a0a4007d3a35ffb71690c98a277c4c24b263d
Silvio Henrique Ferreira <shferreira@me.com>
2020-03-15 00:50:31 +0000

    Revert "Disregard production build check"
    This reverts commit 7cec521f6740b3a688e4a915840f7953b35b11ad.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const posthtmlInlineAssets = require("@shferreira/posthtml-inline-assets");
 
 module.exports = bundler => {
   bundler.on("bundled", (bundle) => {
+    if (process.env.NODE_ENV !== "production" && !process.env.INLINE_ASSETS) return;
+
     const bundles = Array.from(bundle.childBundles).concat([bundle]);
     return Promise.all(bundles.map(async bundle => {
       if (!bundle.entryAsset || bundle.entryAsset.type !== "html") return;
